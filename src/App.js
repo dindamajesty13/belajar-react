abfd2a8cd6e909adb8faf712b349c1e8be69f99d
dindamajesty13 <dindamesti@gmail.com>
2021-07-04 10:09:05 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+//stateless component
+function App() {
+  return (
+    <div className="App">
+      <p>Hi Dinda</p>
+      <h1>Hello World</h1>
+      
+    </div>
+  );
+}
+
+export default App;
